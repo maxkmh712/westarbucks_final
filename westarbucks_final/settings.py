7fa36376db16de04b7f9a59be905e7e9a24de878
minho <maxkmh712@gmail.com>
2021-09-13 20:01:41 +0900

    수정중

--- westarbucks_final/settings.py
+++ westarbucks_final/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib        import Path
 from my_settings    import DATABASES, SECRET_KEY
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bbt_*o+++3v*1@d@vw%ec0ti28^-+9!rvq=w$=kx!9#ta#&8b*'
-
+SECRET_KEY = SECRET_KEY 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -39,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'products'
+    'products',
 ]
 
 MIDDLEWARE = [
